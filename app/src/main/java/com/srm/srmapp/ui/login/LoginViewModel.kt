7ce3404aa4efd6588c5a29c34bd774969b856d26
p014 <pc165@pm.me>
2022-04-03 01:38:42 +0200

    update ui

--- app/src/main/java/com/srm/srmapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/srm/srmapp/ui/login/LoginViewModel.kt
@@ -5,28 +5,52 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.srm.srmapp.Resource
-import com.srm.srmapp.data.models.User
-import com.srm.srmapp.repository.Repository
+import com.srm.srmapp.Utils.launchException
+import com.srm.srmapp.repository.authentication.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+class LoginViewModel @Inject constructor(private val repository: AuthRepository) : ViewModel() {
 
-    private val _loginState: MutableLiveData<Resource<User>> = MutableLiveData()
-    fun getLoginState() = _loginState as LiveData<Resource<User>>
+    private val _loginState: MutableLiveData<Resource<String>> = MutableLiveData()
+    fun getLoginState() = _loginState as LiveData<Resource<String>>
+
+    private val _signupState: MutableLiveData<Resource<String>> = MutableLiveData()
+    fun getSignupState() = _signupState as LiveData<Resource<String>>
+
+    private var formToggle = true
+    fun getFormtoggle() = formToggle
+    fun toggleForm() {
+        formToggle = !formToggle
+    }
 
     fun login(username: String, password: String) {
-        Timber.d("Viewmodel login")
+        Timber.d("login $username $password")
         _loginState.value = Resource.Loading()
-        viewModelScope.launch(Dispatchers.IO) {
-            Timber.d("Viewmodel login coroutine")
-            _loginState.postValue(repository.login(username, password))
+        viewModelScope.launchException(Dispatchers.IO) {
+            val res = repository.login(username, password)
+            _loginState.postValue(res)
         }
     }
 
+
+    fun signup(email: String, name: String, password: String, passwordCheck: String) {
+        Timber.d("signup $email $name $password $passwordCheck")
+        _signupState.value = Resource.Loading()
+        viewModelScope.launchException(Dispatchers.IO) {
+            val res = repository.signup(email, name, password, passwordCheck)
+            _signupState.postValue(res)
+            Timber.d("${res.data}")
+        }
+    }
+
+    fun logout() {
+        viewModelScope.launchException(Dispatchers.IO) {
+            repository.logout()
+        }
+    }
 }
