34f0ee55972d3c40fede393f2dda072b30b6ebe8
p014 <pc165@pm.me>
2022-04-03 01:38:00 +0200

    update module

--- app/src/main/java/com/srm/srmapp/AppModule.kt
+++ app/src/main/java/com/srm/srmapp/AppModule.kt
@@ -1,9 +1,14 @@
 package com.srm.srmapp
 
-import com.srm.srmapp.repository.ApiInterface
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.srm.srmapp.data.UserSession
+import com.srm.srmapp.repository.authentication.AuthInterface
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,37 +20,39 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    private const val BASE_URL = "https://pc165.my.to:8000" // Test url
+    private const val BASE_URL = "https://smart-restaurant-manager.herokuapp.com" // Test url
+
+    @Provides
+    @Singleton
+    fun provideUserSession(@ApplicationContext context: Context, api: AuthInterface): UserSession = UserSession(context, api)
 
     @Provides
     @Singleton
     fun provideHttpClient() = OkHttpClient.Builder()
-        .addInterceptor { // This is needed for the test domain
-            val req = it.request()
-                .newBuilder()
-                .addHeader("device-uuid", "smappdevice")
-                .build()
-            it.proceed(req)
-        }
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .addInterceptor(HttpLoggingInterceptor().setLevel(if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE))
         .build()
 
 
     @Provides
     @Singleton
-    fun provideRetrofitClient(client: OkHttpClient): Retrofit {
-        Timber.d("Create retrofit")
+    fun provideGsonConverter(): Gson = GsonBuilder()
+        .setDateFormat("dd-MM-yyyy'T'HH:mm:ssz")
+        .create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(client: OkHttpClient, gson: Gson): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(client)
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideApiClient(retrofit: Retrofit): ApiInterface {
+    fun provideAuthInterface(retrofit: Retrofit): AuthInterface {
         Timber.d("Create api client")
-        return retrofit.create(ApiInterface::class.java)
+        return retrofit.create(AuthInterface::class.java)
     }
 }
