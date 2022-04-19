0fcfc5481cafb9c03101448df8b60aaefde3788d
p014 <pc165@pm.me>
2022-04-19 15:46:29 +0200

    Delete id

--- app/src/main/java/com/srm/srmapp/data/models/Food.kt
+++ app/src/main/java/com/srm/srmapp/data/models/Food.kt
@@ -1,7 +1,6 @@
 package com.srm.srmapp.data.models
 
 import android.os.Parcelable
-import com.srm.srmapp.R
 import com.srm.srmapp.data.dto.stock.body.FoodObject
 import kotlinx.parcelize.Parcelize
 import timber.log.Timber
@@ -19,12 +18,6 @@ data class Food(val type: FoodType, var foodId: Int, val name: String, val units
 
     companion object {
         fun parseId(id: Int) = when (id) {
-            R.id.btCarne -> FoodType.CARNE
-            R.id.btCereales -> FoodType.CEREALES
-            R.id.btMariscos -> FoodType.MARISCOS
-            R.id.btEspecias -> FoodType.ESPECIAS
-            R.id.btVegetales -> FoodType.VEGETALES
-            R.id.btLacteos -> FoodType.LACTEOS
             else -> {
                 Timber.w("Unkown id $id")
                 FoodType.NONE
--- app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
+++ app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
@@ -1,7 +1,6 @@
 package com.srm.srmapp.data.models
 
 import android.os.Parcelable
-import com.srm.srmapp.R
 import kotlinx.parcelize.Parcelize
 import timber.log.Timber
 
@@ -15,12 +14,6 @@ data class Recipe(val type: RecipeType, val id: Int, val name: String, val price
 
     companion object {
         fun parseId(id: Int) = when (id) {
-            R.id.btEntrantes -> RecipeType.ENTRANTE
-            R.id.btPrimerPlato -> RecipeType.FIRST_PLATE
-            R.id.btSegundoPlato -> RecipeType.SECOND_PLATE
-            R.id.btPostres -> RecipeType.DESERT
-            R.id.btBebidas -> RecipeType.DRINK
-            R.id.btComplementos -> RecipeType.COMPLEMENTS
 
             else -> {
                 Timber.w("Unkown id $id")
