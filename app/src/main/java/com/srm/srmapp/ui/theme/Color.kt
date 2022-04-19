dcbe57e887ec96e05c75f9aea6bafced71565871
p014 <pc165@pm.me>
2022-04-19 21:17:02 +0200

    Add Colors

--- app/src/main/java/com/srm/srmapp/ui/theme/Color.kt
+++ app/src/main/java/com/srm/srmapp/ui/theme/Color.kt
@@ -1,8 +1,11 @@
-package com.srm.srmapp2.ui.theme
+package com.srm.srmapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
+val ButtonColor1 = Color(0xFF3D4545)
+val ButtonColor2 = Color(0xFF271A1A)
+val TextColor = Color(0xFFFFFFFF)
 val Teal200 = Color(0xFF03DAC5)
