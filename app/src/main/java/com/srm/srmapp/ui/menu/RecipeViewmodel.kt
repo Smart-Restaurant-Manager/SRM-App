e281f47c62da6bd3920b1cc78598d5000a1562ed
p014 <pc165@pm.me>
2022-04-22 01:12:27 +0200

    Implement Viewmodel

--- app/src/main/java/com/srm/srmapp/ui/menu/RecipeViewmodel.kt
+++ app/src/main/java/com/srm/srmapp/ui/menu/RecipeViewmodel.kt
@@ -5,57 +5,55 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.srm.srmapp.Resource
 import com.srm.srmapp.Utils.fetchResource
-import com.srm.srmapp.data.models.Food
 import com.srm.srmapp.data.models.Recipe
-import com.srm.srmapp.data.models.Stock
 import com.srm.srmapp.repository.recipes.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class RecipeViewmodel @Inject constructor(private val RecipeRepository: RecipeRepository) : ViewModel() {
-    init {
-        Timber.d("INIT")
-    }
+class RecipeViewmodel @Inject constructor(private val recipeRepository: RecipeRepository) : ViewModel() {
 
     private val _recipeList: MutableLiveData<Resource<List<Recipe>>> = MutableLiveData()
-    fun getRecipeListLiveData() = _recipeList as LiveData<Resource<List<Recipe>>>
+    val recipeList: LiveData<Resource<List<Recipe>>>
+        get() = _recipeList
 
     private val _recipe: MutableLiveData<Resource<Recipe>> = MutableLiveData()
-    fun getRecipeLiveData() = _recipe as LiveData<Resource<Recipe>>
+    val recipe: LiveData<Resource<Recipe>>
+        get() = _recipe
 
-    private val _stockList: MutableLiveData<Resource<List<Stock>>> = MutableLiveData()
-    fun getStockListLiveData() = _stockList as LiveData<Resource<List<Stock>>>
-
-    private val _stock: MutableLiveData<Resource<Stock>> = MutableLiveData()
-    fun getStockLiveData() = _stock as LiveData<Resource<Stock>>
-
-    private val _foodList: MutableLiveData<Resource<List<Food>>> = MutableLiveData()
-    fun getFoodListLiveData() = _foodList as LiveData<Resource<List<Food>>>
-
-    private val _food: MutableLiveData<Resource<Food>> = MutableLiveData()
-    fun getFoodLiveData() = _food as LiveData<Resource<Food>>
-
+    private val _status: MutableLiveData<Resource<String>> = MutableLiveData()
+    val status: LiveData<Resource<String>>
+        get() = _status
 
     fun refreshRecipeList() {
         Timber.d("Call refresh")
         fetchResource(_recipeList) {
-            RecipeRepository.getRecipes()
+            recipeRepository.getRecipes()
         }
     }
 
     fun getRecipeBy(id: Int) {
         fetchResource(_recipe) {
-            RecipeRepository.getRecipe(id)
+            recipeRepository.getRecipe(id)
         }
     }
 
+    fun addRecipe(recipe: Recipe) {
+        fetchResource(_status) {
+            recipeRepository.postRecipe(recipe)
+        }
+    }
 
-//    fun addRecipe(
-//        type: Recipe.RecipeType, id: Int, available: Boolean, name: String, price: Float,
-//        food: Array<Int>,
-//    ): Parcelable {
-//        RecipeRepository.postRecipe(name, price, available, food)
-//    }
+    fun deleteRecipe(id: Int) {
+        fetchResource(_status) {
+            recipeRepository.deleteRecipe(id)
+        }
+    }
+
+    fun putRecipe(recipe: Recipe) {
+        fetchResource(_status) {
+            recipeRepository.putRecipe(recipe)
+        }
+    }
 }
