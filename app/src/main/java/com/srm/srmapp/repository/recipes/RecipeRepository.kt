b84df761873a8f6ae765c3f921474272123dbaa5
p014 <pc165@pm.me>
2022-04-22 01:07:58 +0200

    Update repository

--- app/src/main/java/com/srm/srmapp/repository/recipes/RecipeRepository.kt
+++ app/src/main/java/com/srm/srmapp/repository/recipes/RecipeRepository.kt
@@ -1,37 +1,44 @@
 package com.srm.srmapp.repository.recipes
 
-import com.srm.srmapp.Resource
-import com.srm.srmapp.data.dto.recipe.body.RecipeObject
+import com.srm.srmapp.data.dto.recipe.response.toRecipe
+import com.srm.srmapp.data.dto.recipe.response.toRecipeList
+import com.srm.srmapp.data.models.Recipe
 import com.srm.srmapp.repository.BaseRepository
 import javax.inject.Inject
 
 class RecipeRepository @Inject constructor(private val api: RecipeInterface) : BaseRepository() {
-    suspend fun getRecipes() = safeApiCall({
-        api.getRecipes()
-    }) {
-        it.toRecipeList()
-    }
+    suspend fun getRecipes() =
+        safeApiCall({
+            api.getRecipes()
+        }) {
+            it.toRecipeList()
+        }
 
-    suspend fun getRecipe(id: Int) = safeApiCall({
-        api.getRecipe(id)
-    }) {
-        it.toRecipe()
-    }
+    suspend fun getRecipe(id: Int) =
+        safeApiCall({
+            api.getRecipe(id)
+        }) {
+            it.toRecipe()
+        }
 
-    suspend fun postRecipe(name: String, price: Float, available: Boolean, food: Array<Int>): Resource<String> {
-        return safeApiCall({
-            api.postRecipe(RecipeObject(name, price, available, null))
+    suspend fun postRecipe(recipe: Recipe) =
+        safeApiCall({
+            api.postRecipe(recipe.toJsonObject())
         }) {
             "Recipe added"
         }
-    }
 
-    suspend fun modRecipe(id: Int, name: String, price: Float, available: Boolean, food: Array<Int>): Resource<String> {
-        return safeApiCall({
-            api.putRecipe(id, RecipeObject(name, price, available, null))
+    suspend fun putRecipe(recipe: Recipe) =
+        safeApiCall({
+            api.putRecipe(recipe.id, recipe.toJsonObject())
         }) {
             "Recipe Modified"
         }
-    }
-}
 
+    suspend fun deleteRecipe(id: Int) =
+        safeApiCall({
+            api.deleteRecipe(id)
+        }) {
+            "Recipe deleted"
+        }
+}
