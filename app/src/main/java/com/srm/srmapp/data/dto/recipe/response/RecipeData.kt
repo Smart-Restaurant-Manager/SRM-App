50e3345d3a30d82045926be565190a1b24b5d03a
p014 <pc165@pm.me>
2022-04-22 01:06:42 +0200

    Update recipe response

--- app/src/main/java/com/srm/srmapp/data/dto/recipe/response/RecipeData.kt
+++ app/src/main/java/com/srm/srmapp/data/dto/recipe/response/RecipeData.kt
@@ -6,15 +6,15 @@ data class RecipeData(
     val attributes: RecipeAttributes,
     val id: Int,
     val type: String,
-) {
-    fun toRecipe() = Recipe(
-        type = when (type) {
-            "Entrante" -> Recipe.RecipeType.ENTRANTE
-            "FirstPlate" -> Recipe.RecipeType.FIRST_PLATE
-            "SecondPlate" -> Recipe.RecipeType.SECOND_PLATE
-            "Desert" -> Recipe.RecipeType.DESERT
-            "Bebida" -> Recipe.RecipeType.DRINK
-            "Complementos" -> Recipe.RecipeType.COMPLEMENTS
-            else -> Recipe.RecipeType.NONE
-        }, id = id, name = attributes.name, price = attributes.price)
-}
+)
+
+fun RecipeData.toRecipe() = Recipe(
+    type = when (type) {
+        "Entrante" -> Recipe.RecipeType.ENTRANTE
+        "FirstPlate" -> Recipe.RecipeType.FIRST_PLATE
+        "SecondPlate" -> Recipe.RecipeType.SECOND_PLATE
+        "Desert" -> Recipe.RecipeType.DESERT
+        "Bebida" -> Recipe.RecipeType.DRINK
+        "Complementos" -> Recipe.RecipeType.COMPLEMENTS
+        else -> Recipe.RecipeType.NONE
+    }, id = id, name = attributes.name, price = attributes.price, available = attributes.available)
