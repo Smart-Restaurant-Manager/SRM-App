6e0c6311f680b998ec928fdeef77712ba4d6a1e5
p014 <pc165@pm.me>
2022-04-22 01:07:34 +0200

    Make function as extension

--- app/src/main/java/com/srm/srmapp/data/dto/recipe/response/RecipeListResponse.kt
+++ app/src/main/java/com/srm/srmapp/data/dto/recipe/response/RecipeListResponse.kt
@@ -2,6 +2,6 @@ package com.srm.srmapp.data.dto.recipe.response
 
 data class RecipeListResponse(
     val data: List<RecipeData>,
-) {
-    fun toRecipeList() = data.map { it.toRecipe() }
-}
+)
+
+fun RecipeListResponse.toRecipeList() = data.map { it.toRecipe() }
