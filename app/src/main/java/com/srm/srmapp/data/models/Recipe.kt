f96345c34af83c7bab30cb68c97dd8fbd058df97
p014 <pc165@pm.me>
2022-04-22 01:06:20 +0200

    Update recipe model

--- app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
+++ app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
@@ -1,17 +1,31 @@
 package com.srm.srmapp.data.models
 
 import android.os.Parcelable
+import com.srm.srmapp.data.dto.recipe.body.RecipeFoodObject
+import com.srm.srmapp.data.dto.recipe.body.RecipeObject
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Recipe(val type: RecipeType, val id: Int, val name: String, val price: Float, val food: List<Food>? = null) : Parcelable {
+data class Recipe(
+    val type: RecipeType,
+    val id: Int,
+    val name: String,
+    val price: Float,
+    val available: Boolean,
+    val food: List<Pair<Food, Float>>? = null,
+) :
+    Parcelable {
     enum class RecipeType {
         NONE, ENTRANTE, FIRST_PLATE,
         SECOND_PLATE, DESERT, DRINK,
         COMPLEMENTS
     }
 
+    fun toJsonObject() = RecipeObject(name = name, price = price, available = available, food = food?.map { it.toRecipeFoodObject() })
     override fun toString(): String {
         return "$name  $id  $price "
     }
+
 }
+
+fun Pair<Food, Float>.toRecipeFoodObject() = RecipeFoodObject(food_id = this.first.foodId, this.second)
