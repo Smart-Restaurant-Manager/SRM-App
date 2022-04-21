d9d71e351d11196c17d5c73df752409fc28ac936
p014 <pc165@pm.me>
2022-04-22 00:46:53 +0200

    Update recipe model

--- app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
+++ app/src/main/java/com/srm/srmapp/data/models/Recipe.kt
@@ -2,7 +2,6 @@ package com.srm.srmapp.data.models
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import timber.log.Timber
 
 @Parcelize
 data class Recipe(val type: RecipeType, val id: Int, val name: String, val price: Float, val food: List<Food>? = null) : Parcelable {
@@ -12,18 +11,7 @@ data class Recipe(val type: RecipeType, val id: Int, val name: String, val price
         COMPLEMENTS
     }
 
-    companion object {
-        fun parseId(id: Int) = when (id) {
-
-            else -> {
-                Timber.w("Unkown id $id")
-                Recipe.RecipeType.NONE
-            }
-        }
-    }
-
     override fun toString(): String {
         return "$name  $id  $price "
     }
-
 }
