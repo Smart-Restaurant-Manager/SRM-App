5e756a4951f9bf6c6187106cb9cbdb23b88dfc57
p014 <pc165@pm.me>
2022-04-23 04:20:16 +0200

    Fix

--- app/src/main/java/com/srm/srmapp/ui/MainActivity.kt
+++ app/src/main/java/com/srm/srmapp/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.srm.srmapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.navigation.dependency
@@ -12,7 +11,6 @@ import com.srm.srmapp.ui.menu.RecipeViewmodel
 import com.srm.srmapp.ui.stock.StockViewmodel
 import com.srm.srmapp.ui.theme.SMRappTheme
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -25,15 +23,8 @@ class MainActivity : ComponentActivity() {
             SMRappTheme(false) {
                 DestinationsNavHost(navGraph = NavGraphs.root, dependenciesContainerBuilder = {
                     dependency(userSession)
-                    Timber.d("dependency ${destination.route}")
-                    Timber.d("dependency ${NavGraphs.root.destinations}")
-                    if (NavGraphs.root.destinations.contains(destination)) {
-                        val parentEntry = remember {
-                            navController.getBackStackEntry(NavGraphs.root.route)
-                        }
-                        dependency(hiltViewModel<StockViewmodel>(parentEntry))
-                        dependency(hiltViewModel<RecipeViewmodel>(parentEntry))
-                    }
+                    dependency(hiltViewModel<StockViewmodel>(this@MainActivity))
+                    dependency(hiltViewModel<RecipeViewmodel>(this@MainActivity))
                 })
             }
         }
