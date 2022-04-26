f19a917d06a5367093c2a03646cf609fb4dc8229
p014 <pc165@pm.me>
2022-04-26 22:52:07 +0200

    Remove refresh

--- app/src/main/java/com/srm/srmapp/ui/login/LoginScreen.kt
+++ app/src/main/java/com/srm/srmapp/ui/login/LoginScreen.kt
@@ -8,12 +8,10 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
@@ -35,8 +33,6 @@ import com.srm.srmapp.ui.stock.StockViewmodel
 fun LoginScreen(
     navigator: DestinationsNavigator,
     viewmodel: LoginViewModel = hiltViewModel(),
-    stockViewmodel: StockViewmodel,
-    recipeViewmodel: RecipeViewmodel,
     userSession: UserSession,
 ) {
 
@@ -45,8 +41,6 @@ fun LoginScreen(
         when {
             userState.isEmpty() -> userSession.refresUser()
             userState.isLoading() -> {
-                recipeViewmodel.refreshRecipeList()
-                stockViewmodel.refreshFoodList()
                 SrmSpacedColumn(modifier = Modifier.fillMaxSize()) {
                     CircularProgressIndicator()
                     SrmText(text = "Loading ...")
@@ -58,9 +52,7 @@ fun LoginScreen(
         userSession.logout()
         LoginForm(navigator = navigator,
             viewmodel = viewmodel,
-            userSession = userSession,
-            stockViewmodel = stockViewmodel,
-            recipeViewmodel = recipeViewmodel)
+            userSession = userSession)
     }
 }
 
@@ -68,8 +60,6 @@ fun LoginScreen(
 fun LoginForm(
     navigator: DestinationsNavigator,
     viewmodel: LoginViewModel,
-    stockViewmodel: StockViewmodel,
-    recipeViewmodel: RecipeViewmodel,
     userSession: UserSession,
 ) {
     var user by remember { mutableStateOf("q@q") }
@@ -92,25 +82,29 @@ fun LoginForm(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        SrmTextField(value = user,
+        SrmTextField(
+            value = user,
             label = stringResource(id = R.string.user_mail),
             enabled = loginState !is Resource.Loading,
             isError = loginState is Resource.Error,
             onValueChange = { user = it },
         )
 
-        SrmTextField(value = password,
+        SrmTextField(
+            value = password,
             label = stringResource(id = R.string.password),
             enabled = loginState !is Resource.Loading,
             isError = loginState is Resource.Error,
             onValueChange = { password = it },
             modifier = Modifier
                 .padding(0.dp, 20.dp),
-            )
+        )
 
-        SrmButton(onClick = {
-            viewmodel.login(user, password)
-        }, text = stringResource(id = R.string.login),
+        SrmButton(
+            onClick = {
+                viewmodel.login(user, password)
+            },
+            text = stringResource(id = R.string.login),
             enabled = loginState !is Resource.Loading,
         )
 
@@ -121,9 +115,7 @@ fun LoginForm(
             }
         }
 
-        if (loginState is Resource.Loading) {
-            recipeViewmodel.refreshRecipeList()
-            stockViewmodel.refreshFoodList()
+        if (loginState.isLoading()) {
             CircularProgressIndicator()
         }
 
