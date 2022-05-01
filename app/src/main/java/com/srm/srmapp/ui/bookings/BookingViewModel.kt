a85528b9a28de8df88824727205934d08fb47d53
p014 <pc165@pm.me>
2022-05-01 22:42:44 +0200

    ViewModels

--- app/src/main/java/com/srm/srmapp/ui/bookings/BookingViewModel.kt
+++ app/src/main/java/com/srm/srmapp/ui/bookings/BookingViewModel.kt
@@ -1,4 +1,26 @@
 package com.srm.srmapp.ui.bookings
 
-class BookingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.srm.srmapp.Resource
+import com.srm.srmapp.Utils.fetchResource
+import com.srm.srmapp.data.models.Booking
+import com.srm.srmapp.data.models.Recipe
+import com.srm.srmapp.repository.bookings.BookingRepository
+import com.srm.srmapp.ui.common.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+@HiltViewModel
+class BookingViewModel @Inject constructor(val bookingRepository: BookingRepository) : BaseViewModel() {
+    private val _recipeList: MutableLiveData<Resource<List<Recipe>>> = MutableLiveData()
+    val recipeList: LiveData<Resource<List<Recipe>>>
+        get() = _recipeList
+
+    fun addBooking(name: String, amountPeople: Int, date: String, telephone: String, mail: String, table: String) {
+        fetchResource(this._status) {
+            bookingRepository.postBooking(Booking(id = null, name, mail, telephone, LocalDateTime.now(), amountPeople, table))
+        }
+    }
 }
