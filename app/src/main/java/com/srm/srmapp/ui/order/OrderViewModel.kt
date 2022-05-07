33753f686d483021125448ddb25e89b2b81739bf
Ricardo <samaelmrnstar@gmail.com>
2022-05-07 19:01:42 +0200

    Order View Model

--- app/src/main/java/com/srm/srmapp/ui/order/OrderViewModel.kt
+++ app/src/main/java/com/srm/srmapp/ui/order/OrderViewModel.kt
@@ -2,86 +2,75 @@ package com.srm.srmapp.ui.order
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.srm.srmapp.Resource
 import com.srm.srmapp.Utils.fetchResource
 import com.srm.srmapp.data.models.Order
-import com.srm.srmapp.data.models.Recipe
+import com.srm.srmapp.repository.orders.OrdersInterface
 import com.srm.srmapp.repository.orders.OrdersRepository
-import com.srm.srmapp.repository.recipes.RecipeRepository
-import com.srm.srmapp.repository.stock.StockRepository
-import com.srm.srmapp.ui.common.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
+
 @HiltViewModel
-class OrderViewModel @Inject constructor(
-    private val api: OrdersRepository,
-    private val recipeApi: RecipeRepository,
-    private val foodApi: StockRepository,
-) : BaseViewModel() {
-    private val _orderList = MutableLiveData<Resource<List<Order>>>(Resource.Empty())
+class OrderViewmodel @Inject constructor(private val ordersRepository: OrdersRepository) : ViewModel() {
+    private val _orderList: MutableLiveData<Resource<List<Order>>> = MutableLiveData()
     val orderList: LiveData<Resource<List<Order>>>
         get() = _orderList
 
-    private val _orderStatus = MutableLiveData<Order.Status>()
-    val orderStatus: LiveData<Order.Status>
-        get() = _orderStatus
-
-    private val _order = MutableLiveData<Resource<Order>>(Resource.Empty())
+    private val _order: MutableLiveData<Resource<Order>> = MutableLiveData()
     val order: LiveData<Resource<Order>>
         get() = _order
+    private val _status: MutableLiveData<Resource<String>> = MutableLiveData()
+    val status: LiveData<Resource<String>>
+        get() = _status
 
-    private val _recipe = MutableLiveData<Resource<Recipe>>(Resource.Empty())
-    val recipe: LiveData<Resource<Recipe>>
-        get() = _recipe
 
-    fun clearOrder() {
-        _order.value = Resource.Empty()
-    }
-
-    fun setOrderStatus(status: Order.Status) {
-        _orderStatus.value = status
-    }
-
-    fun clearOrderStatus() {
-        _orderStatus.value = Order.Status.None()
-    }
-
-    fun refreshOrder() {
+    fun refreshOrderList() {
+        Timber.d("Call refresh")
         fetchResource(_orderList) {
-            orderStatus.value?.let {
-                if (it is Order.Status.None)
-                    return@let api.getOrders()
-                api.getOrderByStatus(it)
-            } ?: api.getOrders()
+            ordersRepository.getOrders()
         }
     }
 
-    fun getOrderRecipe(order: Order) {
-        fetchResource(_order) {
-            api.getOrder(order.orderId)
+    fun getOrderBy(id: Int){
+        fetchResource(_order){
+            ordersRepository.getOrder(id)
+        }
+    }
+    fun addOrder(order: Order){
+        fetchResource(_status, onSuccess = {
+            refreshOrderList()
+        }){
+            ordersRepository.postOrder((order))
         }
     }
 
-    fun getRecipe(recipeId: Int) {
-        fetchResource(_recipe) {
-            recipeApi.getRecipe(recipeId)
-        }
-    }
-
-    fun changeOrderStatus(status: Order.Status) {
-        _order.value?.data?.let { order ->
-            order.status = status
-            fetchResource(_status,
-                onSuccess = { _ ->
-                    _order.postValue(Resource.Success(order))
-                }) {
-                api.putOrder(order)
+    fun deleteOrder(id: Int){
+        fetchResource(_status, onSuccess = {
+            _orderList.value?.data?.toMutableList()?.let {
+                list -> list.removeIf{it.orderId == id}
+                _orderList.postValue(Resource.Success(list.toList()))
             }
+        }){
+            ordersRepository.deleteOrder(id)
         }
     }
 
-    suspend fun getFoodName(id: Int) {
-        foodApi.getFood(id)
+    fun getOrdersByStatus(status: Order.Status){
+        fetchResource(_orderList){
+            ordersRepository.getOrderByStatus(status)
+        }
     }
+
+    fun editOrder(order: Order){
+        fetchResource(_status){
+            ordersRepository.putOrder(order)
+        }
+    }
+
+
+
 }
+
