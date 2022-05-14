388e25b25328cd314b4f3558a38f843244f9db1e
p014 <pc165@pm.me>
2022-05-14 20:26:26 +0200

    Refactor Order UI

--- app/src/main/java/com/srm/srmapp/ui/order/OrderScreen.kt
+++ app/src/main/java/com/srm/srmapp/ui/order/OrderScreen.kt
@@ -1,17 +1,9 @@
 package com.srm.srmapp.ui.order
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -19,14 +11,8 @@ import com.srm.srmapp.AppModule
 import com.srm.srmapp.R
 import com.srm.srmapp.Resource
 import com.srm.srmapp.data.models.Order
-import com.srm.srmapp.data.models.Recipe
 import com.srm.srmapp.ui.common.*
-import com.srm.srmapp.ui.theme.spacerWitdh
-import kotlinx.coroutines.delay
-import timber.log.Timber
-import kotlin.time.Duration.Companion.seconds
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 @Destination
 fun OrderScreen(
@@ -36,150 +22,65 @@ fun OrderScreen(
     val orderListState by viewmodel.orderList.observeAsState(Resource.Empty())
     if (orderListState.isEmpty()) viewmodel.refreshOrder()
 
-    val order by viewmodel.order.observeAsState(Resource.Empty())
     val orderStatus by viewmodel.orderStatus.observeAsState(Order.Status.None())
 
-    LaunchedEffect(key1 = orderStatus, block = {
-        while (true) {
-            delay(30.seconds)
-            Timber.d("Refresh orders")
-            viewmodel.refreshOrder()
-        }
-    })
 
-    var orderDialog by remember { mutableStateOf(false) }
-    var searchDialog by remember { mutableStateOf(false) }
-    val orderList = remember(orderListState.data) { orderListState.data ?: emptyList() }
+    // search engine properties
+    val searchProperties = SrmSearchProperties<Order>(
+        searchPredicate = viewmodel.predicate,
+        indexPredicate = { it, found -> it.orderId == found.orderId },
+        searchLabel = "Buscar pedidos",
+        startSearchText = { "${it.orderId}" },
+        endSearchText = { "Taula ${it.booking?.table}" })
 
-    var addDialog by remember { mutableStateOf(false) }
+    // dialog content
+    val crudDialogContent = SrmCrudDialogContent<Order>(
+        editDialogContent = { item ->
+            // TODO
+            SrmText(text = "Todo")
+        },
+        addDialogContent = {
+            // TODO set to null to disable
+            SrmText(text = "Todo")
+        },
+        deleteDialogContent = {
+            // TODO confirmation with yes/no
+            SrmText(text = "Todo")
+        },
+        moreDialogContent = {
+            // TODO
+            SrmText(text = "Todo")
+        },
+    )
 
-    Column(modifier = Modifier
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        SrmAddTitleSearch(title = "Orders",
-            onClickAdd = { addDialog = true },
-            onClickBack = { navigator.popBackStack() },
-            onClickSearch = { searchDialog = true })
-        DropDownChangeStatus(text = stringResource(id = orderStatus.getStringId()), onStatusChange = {
-            viewmodel.setOrderStatus(it)
-            viewmodel.refreshOrder()
+    SrmListWithCrudActions(
+        title = stringResource(id = R.string.reservas),
+        itemList = orderListState.data ?: emptyList(),
+        onAddDialog = {
+            // TODO
+            SrmText(text = "Todo")
+        },
+        onBack = { navigator.navigateUp() },
+        onRefresh = { viewmodel.refreshOrder() },
+        refresState = rememberSwipeRefreshState(isRefreshing = orderListState.isLoading()),
+        itemKey = { it.orderId },
+        listItemStartText = { "${it.orderId}\nTaula: ${it.booking?.table}" },
+        listItemEndText = {
+            val status: String = if (orderStatus is Order.Status.None) it.status.toString() else ""
+            val date = it.booking?.date?.format(AppModule.dateTimeFormatter)
+            "${status}\n${date}"
+        },
+        searchProperties = searchProperties,
+        crudDialogContent = crudDialogContent,
+        baseViewModel = viewmodel,
+        contentBefore = {
+            DropDownChangeStatus(text = stringResource(id = orderStatus.getStringId()), onStatusChange = {
+                viewmodel.setOrderStatus(it)
+                viewmodel.refreshOrder()
+            })
         })
-        SwipeRefresh(
-            state = rememberSwipeRefreshState(isRefreshing = orderListState.isLoading()),
-            onRefresh = { viewmodel.refreshOrder() }) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(orderList, key = { it.orderId }) {
-                    OrderItem(order = it,
-                        orderStatus = orderStatus,
-                        onClick = {
-                            viewmodel.getOrderRecipe(it)
-                            orderDialog = true
-                        })
-                }
-            }
-        }
-    }
-
-
-    if (orderDialog && order.isSuccess()) {
-        order.data?.let {
-            OrderPopUp(
-                it,
-                onDismissRequest = {
-                    orderDialog = false
-                },
-                onDelete = {
-                    viewmodel.deleteOrder(it)
-                    orderDialog = false
-                },
-                onStatusChange = { status ->
-                    viewmodel.changeOrderStatus(it, status)
-                },
-                onModify = {
-                    // TODO
-                }
-            )
-        }
-    }
-
-    if (addDialog) {
-        // TODO
-    }
-
-    if (searchDialog) {
-        SrmSearch(items = orderList,
-            label = "Buscar pedidos",
-            onDismissRequest = { searchDialog = false },
-            predicate = viewmodel.predicate) { order ->
-            var dialogItemState by remember { mutableStateOf(false) }
-            OrderItem(order = order, orderStatus = orderStatus, minimal = true) {
-                dialogItemState = true
-            }
-            if (dialogItemState) {
-                OrderPopUp(
-                    order, onDismissRequest = { dialogItemState = false },
-                    onDelete = {
-                        viewmodel.deleteOrder(order)
-                        dialogItemState = false
-                    },
-                    onStatusChange = { status ->
-                        viewmodel.changeOrderStatus(order, status)
-                    },
-                    onModify = {
-                        //TODO
-                    }
-                )
-            }
-        }
-    }
 }
 
-
-@Composable
-fun OrderPopUp(
-    order: Order,
-    onDismissRequest: () -> Unit,
-    onDelete: () -> Unit,
-    onStatusChange: (Order.Status) -> Unit,
-    onModify: () -> Unit,
-) {
-    SrmDialog(onDismissRequest = onDismissRequest) {
-        SrmText(text = "Plats: ")
-        order.recipeList.forEachIndexed { index, recipe ->
-            Column(modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()) {
-                SrmText(text = "Nom ${recipe.recipeId}")
-                SrmText(text = "Quantity ${recipe.quantity}")
-                SrmText(text = "Tipus ${stringResource(id = Recipe.getResource(recipe.type))}")
-            }
-        }
-
-        SrmSelectableRow(
-            horizontalArrangement = Arrangement.Start,
-            onClick = onDelete) {
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            Icon(painter = painterResource(id = R.drawable.ic_baseline_delete_24), contentDescription = stringResource(id = R.string.delete))
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            SrmText(text = stringResource(R.string.delete))
-        }
-
-        SrmSelectableRow(
-            horizontalArrangement = Arrangement.Start,
-            onClick = onModify) {
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            Icon(painter = painterResource(id = R.drawable.ic_baseline_edit_24), contentDescription = stringResource(id = R.string.modificar))
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            SrmText(text = stringResource(R.string.modificar))
-        }
-        SrmSpacedRow {
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            Icon(painter = painterResource(id = R.drawable.ic_baseline_edit_24), contentDescription = stringResource(id = R.string.modificar))
-            Spacer(modifier = Modifier.width(spacerWitdh))
-            DropDownChangeStatus(text = "Estat actual: ${stringResource(id = order.status.getStringId())}", onStatusChange = onStatusChange)
-        }
-    }
-}
 
 @Composable
 fun DropDownChangeStatus(text: String, onStatusChange: (Order.Status) -> Unit) {
@@ -188,16 +89,4 @@ fun DropDownChangeStatus(text: String, onStatusChange: (Order.Status) -> Unit) {
     ) {
         SrmText(text = stringResource(id = it.getStringId()))
     }
-}
-
-@Composable
-fun OrderItem(order: Order, orderStatus: Order.Status, minimal: Boolean = false, onClick: () -> Unit = {}) {
-    val status: String = if (orderStatus is Order.Status.None) stringResource(id = order.status.getStringId()) else ""
-    val date = order.booking?.date?.format(AppModule.dateTimeFormatter)
-    val table = order.booking?.table ?: ""
-
-    if (minimal)
-        SrmListItem(startText = "${order.orderId}", endText = "Taula $table", onClick = onClick)
-    else
-        SrmListItem(startText = "${order.orderId}\nTaula: $table", endText = "$status\n$date", onClick = onClick)
 }
